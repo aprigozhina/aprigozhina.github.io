bc685440fd15afdb528e5093699680bb41276662
aprigozhina <48488205+aprigozhina@users.noreply.github.com>
2019-03-28 18:44:24 -0500

    Add files via upload

---
+++ lingvoMap/lingvo.js
@@ -0,0 +1,11 @@
+let myMap = L.map('lingvoMap').setView([30.50, -91.00], 9)
+
+// Basemap tiles
+var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+}).addTo(myMap);
+
+let lingvoData = 'https://github.com/aprigozhina/aprigozhina.github.io/blob/master/lingvoMap/lingvoMap.geojson'
+jQuery.getJSON(lingvoData, function (data) {
+  L.geoJSON(data).addTo(myMap)
+})
