dbad640efc5d1038d4dc868905a2902b24a237ff
aprigozhina <48488205+aprigozhina@users.noreply.github.com>
2019-05-03 13:47:48 -0500

    Add files via upload

---
+++ macrosystems/miami/miami.js
@@ -0,0 +1,37 @@
+//  MAP 1
+
+let myMap = L.map('miami_sites').setView([25.75, -80.5], 10)
+
+// Basemap tiles
+let openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap)
+let esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(myMap);
+let esriWorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+}).addTo(myMap);
+
+// add layer control
+let basemaps = {
+  'OpenStreetMap': openStreetMap,
+	'ESRI Imagery': esri_WorldImagery,
+  'ESRI Topo Map': esriWorldTopoMap
+}
+
+L.control.layers(basemaps, {
+}).addTo(myMap)
+
+
+
+//  MAP 2
+
+let pMap = L.map('miami_parking').setView([25.75, -80.5], 10)
+
+// Basemap tiles
+let openStreetMap2 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(pMap)
