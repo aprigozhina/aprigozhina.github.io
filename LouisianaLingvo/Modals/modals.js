dec2811074686c1c834cf9b76555a04b9a14fe09
aprigozhina <48488205+aprigozhina@users.noreply.github.com>
2019-06-20 16:55:07 -0500

    Add files via upload

--- LouisianaLingvo/Modals/modals.js
+++ LouisianaLingvo/Modals/modals.js
@@ -9,57 +9,126 @@ let OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 // set variables for layer-control and info pane
 let answerSet1
 let answerSet2
-let vowels
 let sentence1 = L.layerGroup().addTo(myMap)
 let sentence2 = L.layerGroup().addTo(myMap)
-let speakers = L.layerGroup().addTo(myMap)
+let parishesLayer = L.layerGroup().addTo(myMap)
+let zipCodeLayer = L.layerGroup().addTo(myMap)
+
+// create panes
+myMap.createPane('markers')
+myMap.getPane('markers').style.zIndex = 650
 
 // Add GeoJSON with markers
 // Layer 1
-let lingvoData = 'https://aprigozhina.github.io/LouisianaLingvo/data.geojson'
+let lingvoData = 'https://aprigozhina.github.io/LouisianaLingvo/Modals/modals1.geojson'
 jQuery.getJSON(lingvoData, function (data) {
 	answerSet1 = L.geoJson(data, {
 		 onEachFeature: onEachFeature1,
 			pointToLayer: function(feature,latlng){
 				var colors = { // variable properties go in curly braces
-					"Yes": "#1a9850",
-					"No": "#d73027"
+					"TRUE": "#1a9850",
+					"FALSE": "#d73027"
 				};
 				return L.circleMarker(latlng, {
 					radius: 7,
-					fillColor: colors[feature.properties.Answer],
-					color: colors[feature.properties.Answer],
+					fillColor: colors[feature.properties.answer],
+					color: colors[feature.properties.answer],
 					weight: 1,
 					opacity: 1,
 					fillOpacity: 0.6,
-					clickable: true
+					clickable: true,
+					pane: 'markers'
 				});
 			}
 	}).addTo(myMap)
 })
 
 // Layer 2
-let lingvoData2 = 'https://aprigozhina.github.io/LouisianaLingvo/data2.geojson'
+let lingvoData2 = 'https://aprigozhina.github.io/LouisianaLingvo/Modals/modals1.geojson'
 jQuery.getJSON(lingvoData2, function (data) {
 	answerSet2 = L.geoJson(data, {
 		 onEachFeature: onEachFeature2,
 			pointToLayer: function(feature,latlng){
 			var colors = {
-					"Yes": "#66c2a5",
-					"No": "#f46d43"
+					"TRUE": "#66c2a5",
+					"FALSE": "#f46d43"
 			};
 			return L.circleMarker(latlng, {
 				radius: 7,
-				fillColor: colors[feature.properties.Answer],
-				color: colors[feature.properties.Answer],
+				fillColor: colors[feature.properties.answer],
+				color: colors[feature.properties.answer],
 				weight: 1,
 				opacity: 1,
 				fillOpacity: 0.6,
-				clickable: true
+				clickable: true,
+				pane: 'markers'
 			});
 		}
 	}).addTo(myMap)
 })
+
+// Parishes
+let parishUrl = 'https://aprigozhina.github.io/LouisianaLingvo/ParishSimple.geojson'
+jQuery.getJSON(parishUrl, function (data) {
+	let parishes = function (feature) { // this function returns an object
+	//	let males = feature.properties.MALES
+	//	let females = feature.properties.FEMALES
+	//		let genderComposition = males / females * 100
+			let parishColor = '#bdc9e1'
+	//			if ( genderComposition < 100 ) {stateColor = '#67a9cf'}
+	//			if ( genderComposition < 97 ) { stateColor = '#02818a' }
+						return {
+							fillColor: parishColor,
+							color: '#808B96', //use the color variable above for the value
+						 	weight: 1,
+						 	fillOpacity: 0.4,
+						 	dashArray: '3'
+						}
+		}
+				let parishesOptions = {
+					style: parishes,
+		  		onEachFeature: onEachFeature6
+		 		}
+	L.geoJSON(data, parishesOptions).addTo(myMap)
+})
+
+// add pop-ups
+let onEachFeature6 = function (feature, layer) {
+			let name = feature.properties.NAMELSAD
+				 	layer.bindPopup(name)
+		parishesLayer.addLayer(layer)
+ }
+
+ // ZIP codes
+ let zipUrl = 'https://aprigozhina.github.io/LouisianaLingvo/ZIPSimple.geojson'
+ jQuery.getJSON(zipUrl, function (data) {
+ 	let zipCodes = function (feature) { // this function returns an object
+ 	//	let males = feature.properties.MALES
+ 	//	let females = feature.properties.FEMALES
+ 	//		let genderComposition = males / females * 100
+ 			let zipColor = '#A2D9CE'
+ 	//			if ( genderComposition < 100 ) {stateColor = '#67a9cf'}
+ 	//			if ( genderComposition < 97 ) { stateColor = '#02818a' }
+ 						return {
+ 							fillColor: zipColor,
+ 							color: '#808B96', //use the color variable above for the value
+ 						 	weight: 0.5,
+ 						 	fillOpacity: 0.4
+ 						}
+ 		}
+ 				let zipOptions = {
+ 					style: zipCodes,
+ 		  		onEachFeature: onEachFeature7
+ 		 		}
+ 	L.geoJSON(data, zipOptions).addTo(myMap)
+ })
+
+ // add pop-ups
+ let onEachFeature7 = function (feature, layer) {
+ 			let name = feature.properties.ZCTA5CE10
+ 				 	layer.bindPopup('ZIP: ' + name)
+ 		zipCodeLayer.addLayer(layer)
+  }
 
 // make highlight and restore
 function onEachFeature1(feature, layer){
@@ -97,45 +166,17 @@ function resetHighlight(e) {
 		info.update()
 }
 
-// Layer 3
-let speakersUrl = 'https://aprigozhina.github.io/LouisianaLingvo/tabletry.geojson'
-jQuery.getJSON(speakersUrl, function (data) {
-	vowels = L.geoJson(data, {
-		 onEachFeature: onEachFeature3,
-			pointToLayer: function(feature,latlng){
-				var colors = { // variable properties go in curly braces
-					"Never changed":"#1a9850",
-					"Current":"#1a9850"
-				};
-				return L.circleMarker(latlng, {
-					radius: 7,
-					fillColor: colors[feature.properties.ziptype],
-					color: colors[feature.properties.ziptype],
-					weight: 1,
-					opacity: 1,
-					fillOpacity: 0.6,
-					clickable: true
-				});
-			}
-	}).addTo(myMap)
-})
-
-// add pop-ups
-let onEachFeature3 = function (feature, layer) {
-			let tablePopup = feature.properties.table
-			 	layer.bindPopup(tablePopup)
-		speakers.addLayer(layer)
- }
-
 // add layer control
 let baseLayers = {
-	"OSM": OpenStreetMap_Mapnik
+
+	"Sentence 1": sentence1,
+	"Sentence 2": sentence2
 }
 
 let overlays = {
-	"Sentence 1": sentence1,
-	"Sentence 2": sentence2,
-	"Speakers": speakers
+	"OSM": OpenStreetMap_Mapnik,
+	"Parishes": parishesLayer,
+	"ZIP codes": zipCodeLayer
 }
 
 L.control.layers(baseLayers, overlays, {
@@ -154,14 +195,16 @@ info.onAdd = function (map) {
 
 // update the control based on feature properties
 info.update = function (props) {
-    this._div.innerHTML = '<h4>Sentence use survey</h4>' +  (props ?
-        '<b>Answer: </b>' + props.Answer +
-				'<br><b>Gender: </b>' + props.Gender +
-				'<br><b>Age: </b>' + props.Age +
-				'<br><b>Education: </b>' + props.Education +
-				'<br><b>Race: </b>' + props.Race +
-				'<br><b>Place raised: </b>' + props.Birth_plac +
-				'<br><b>Currently lives: </b>' + props.Live_place
+    this._div.innerHTML = '<h4>Double Modals Survey</h4>' +  (props ?
+        '<b>Answer: </b>' + props.answer +
+				'<br><b>ZIP code: </b>' + props.zipcode +
+				'<br><b>Gender: </b>' + props.gender +
+				'<br><b>Age: </b>' + props.age +
+				'<br><b>Ethnicity: </b>' + props.ethnicity +
+				'<br><b>Education: </b>' + props.education +
+				'<br><b>Mother Education: </b>' + props.mother_ed +
+				'<br><b>Father Education: </b>' + props.father_ed +
+				'<br><b>Stay in Louisiana: </b>' + props.stayInLA
         : 'Hover over a point')
 }
 
